d13c935951c2c55fb2d31365f2977347ec83aa27
mahmoodAlzaki <161972264+mahmoodAlzaki@users.noreply.github.com>
2025-12-04 09:00:03 +0300

    Update index.php

--- src/resources/api/index.php
+++ src/resources/api/index.php
@@ -1,7 +1,5 @@
 <?php
-// ============================================================================
-// HEADERS AND INITIALIZATION
-// ============================================================================
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
