4eaced8f397e8449f030ddab6640ec838a7bf3c9
Buali03 <alibuali3660@gmail.com>
2025-12-13 12:45:21 +0300

    Initialize session for user authentication

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if(!isset($_SESSION['user'])) {
+    $_SESSION['user'] = null;
+}
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
