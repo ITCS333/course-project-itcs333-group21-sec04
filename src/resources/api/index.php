3d100f55aa8620453631be7e73776c82824e2d05
mahmoodAlzaki <161972264+mahmoodAlzaki@users.noreply.github.com>
2025-12-17 02:13:21 +0300

    Initialize session and set CORS headers

--- src/resources/api/index.php
+++ src/resources/api/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$_SESSION['initialized'] = true;
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
